f189d08a1ad59eda9e844ac509d1ec452d9d1288
millooo94 <camillomusmeciunict@gmail.com>
2023-01-23 15:24:22 +0100

    refactor all

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PageController@home')->name('home');
 Route::get('/dashboard', 'PageController@dashboard')->name('dashboard');
 
-Route::resource('comics', 'ComicController');
+Route::resource('admin.comics', 'ComicController');
